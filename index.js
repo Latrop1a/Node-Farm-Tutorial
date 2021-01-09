516425a798e43a313903e25f5dafb03d213b1376
Latrop <32779759+Latrop1a@users.noreply.github.com>
2021-01-09 18:32:57 +0100

    made module, slugified the urls

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
+const slugify = require('slugify');
 
 //////////////////////
 //Files
@@ -12,7 +14,7 @@ console.log(textIn);
 const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written');
-
+no
 // Non-blocking async
 fs.readFile('./txt/start.txt', (err, data1) => {
   if (err) return console.log(err);
@@ -39,22 +41,17 @@ const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   'utf-8'
 );
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data2.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-// template building
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
+const addSlugs = (slugArray) => {
+  let x = 0;
+  slugArray.forEach((element) => {
+    dataObj[x].slug = element;
+    x++;
+  });
 };
 
 const server = http.createServer((req, res) => {
@@ -71,7 +68,7 @@ const server = http.createServer((req, res) => {
 
     // PRODUCT PAGES
   } else if (pathname === '/product') {
-    const product = dataObj[query.id];
+    const product = dataObj[slugs.indexOf(query.id)];
     const productHTML = replaceTemplate(tempProduct, product);
     res.end(productHTML);
 
